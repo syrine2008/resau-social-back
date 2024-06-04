71b5e1e1aab7380672bbca67032d824c44ac44b5
syrine2008 <zaffati.sirine@gmail.com>
2024-06-04 16:49:05 +0200

    init

---
+++ reseau-social/src/test/java/com/example/rsi/ReseauSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReseauSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
